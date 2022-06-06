0682908e1179bdd6faa49d26847b89ab24e4bcdc
BobbiliAnusha <106606897+BobbiliAnusha@users.noreply.github.com>
2022-06-06 13:32:15 +0530

    Update dairy.js

--- routes/dairy.js
+++ routes/dairy.js
@@ -6,5 +6,6 @@ router.post('/:userid', dairyController.addEntryToDairy);
 router.put('/:userid', dairyController.editEntry);
 router.delete('/:userid', dairyController.deleteEntry);
 router.get('/:userid', dairyController.entryById);
+router.get('/date/:date',dairyController.entryBYDate)
 
 module.exports = router;
